9b5a7130685b7ab2998e216ff221f54d05578ab5
ammar-elmesaly <amarmazn4@gmail.com>
2025-10-06 15:46:35 +0300

    feat: add root endpoint to provide API information

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.json({
+    "name": "Quran-API",
+    "version": "1.0.0",
+    "description": "A RESTful API for accessing and exploring Quranic text programmatically.",
+    "docs": "https://github.com/ammar-elmesaly/quran-api#readme",
+    "endpoints": {
+      "quran": "/quran/:surah/:verse",
+      "tafsir": "/quran/:surah/:verse/tafsir/:tafsir",
+      "user": "/users"
+    },
+  });
+});
+
 app.use(allRoutes);
 
 app.listen(PORT, () => {
