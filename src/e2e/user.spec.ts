9b6f1591e70cd193e16000ece3f449c41031a813
ammar-elmesaly <amarmazn4@gmail.com>
2025-10-11 21:04:19 +0300

    feat: enhance user login tests and cleanup database connection

--- src/e2e/user.spec.ts
+++ src/e2e/user.spec.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 
 import { app } from "../app";
 import { deleteUserByUsername } from "../services/user";
+import pool from "../services/db";
 
 
 describe('Register user', () => {
@@ -30,22 +31,66 @@ describe('Register user', () => {
   });
 
   afterAll(async () => {
-    await deleteUserByUsername("john_doe");
+    await deleteUserByUsername("john_doe");  // Deleting the user after tests
   });
 });
 
-/*
 describe('Login user', () => {
+
+  beforeAll(async () => {
+    await request(app).post('/users/new').send({
+      username: "john_doe",
+      password: "ValidPassword123"
+    });
+  });
+
+  let authCookie: string;
+
   it('should return 401 if not authenticated', async () => {
     const res = await request(app).get('/quran/saved');
     expect(res.status).toBe(401);
     expect(res.body).toEqual({"message":"Access Denied"});
   });
 
-  it("should return 400 if user doesn't exist", async () => {
-    const res = await request(app).post('/user/login');
+  it("should return 401 if user doesn't exist", async () => {
+    const res = await request(app).post('/users/login').send({
+      username: "doesnt_exist",
+      password: "LoremIpsum123"
+    });
     expect(res.status).toBe(401);
-    expect(res.body).toEqual({"message":"Access Denied"});
+    expect(res.body).toEqual({"message":'Invalid credentials'});
   });
+
+  it("should login the user", async () => {
+    const res = await request(app).post('/users/login').send({
+      username: "john_doe",
+      password: "ValidPassword123"
+    });
+
+    authCookie = res.headers['set-cookie']![0]!;
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      message: "Login successful",
+      user: {
+        id: expect.any(Number),
+        username: "john_doe",
+        created_at: expect.any(String)
+      }
+    });
+    expect(authCookie).toBeTruthy();
+  });
+
+  it("should access protected routes", async () => {
+    const res = await request(app)
+    .get('/quran/saved')
+    .set('Cookie', authCookie);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([]);
+  });
+
+  afterAll(async () => {
+    await deleteUserByUsername("john_doe");  // Deleting the user after tests
+    pool.end();
+  })
 });
-*/
